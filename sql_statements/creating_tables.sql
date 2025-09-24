d857d0edc493b7d96adeb3bfff336fefa1bd829d
Kim Hummel <hummel.kimberly@gmail.com>
2025-09-24 19:21:35 -0400

    started EDA

---
+++ sql_statements/creating_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE authors (
+    author_id TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
+);
+
+INSERT INTO authors(author_id, first_name, last_name)
+Values ("10f88232-1ae7-4d88-a6a2-dfcebb22a596","Harper","Lee")
+
+SELECT * FROM authors;
---
+++ sql_statements/second_try.sql
@@ -0,0 +1,18 @@
+--CREATE TABLE authors (author_id NOT NULL PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT NOT NULL);
+
+--INSERT INTO authors(author_id, first_name, last_name)
+--Values ("10f88232-1ae7-4d88-a6a2-dfcebb22a596","Harper","Lee")
+
+--INSERT INTO authors(author_id, first_name, last_name) VALUES
+--("e0b75863-866d-4db4-85c7-df9bb8ee6dab","F. Scott","Fitzgerald"),
+--("7b144e32-7ff4-4b58-8eb0-e63d3c9f9b8d","Aldous","Huxley")
+
+--INSERT INTO authors(author_id, first_name, last_name) VALUES
+--("8d8107b6-1f24-481c-8a21-7d72b13b59b5","J.D.","Salinger"),
+--("0cc3c8e4-e0c0-482f-b2f7-af87330de214","Ray","Bradbury"),
+--("4dca0632-2c53-490c-99d5-4f6d41e56c0e","Jane","Austen"),
+--("16f3e0a1-24cb-4ed6-a50d-509f63e367f7","J.R.R.","Tolkien"),
+--("06cf58ab-90f1-448d-8e54-055e4393e75c","J.R.R.","Tolkien"),
+--("6b693b96-394a-4a1d-a4e2-792a47d7a568","J.K.","Rowling")
+
+SELECT * FROM authors
